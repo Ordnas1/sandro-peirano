fbec651c7099e62fbe62c372ba16ed72061a7d7a
Sandro Marcelo Peirano Gozalvez <sandro.peirano@devsu.com>
2025-08-17 19:19:47 -0500

    Add testing with jest

---
+++ setup-jest.ts
@@ -0,0 +1,5 @@
+import { TestBed } from "@angular/core/testing";
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
+
+// TODO remove this when jest-preset-angular is updated to support zoneless angular
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
